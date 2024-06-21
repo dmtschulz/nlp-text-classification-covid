e7721b7a9e98a0ad0776d8903414c1447406fc4b
Dimitrij Schulz <dmtschulz@gmail.com>
2024-06-21 22:08:25 +0200

    small commit

--- NLP/1 Project/classification.py
+++ NLP/1 Project/classification.py
@@ -23,20 +23,43 @@ def train_and_evaluate_nb(vectorizer, X_train, X_test, y_train, y_test, filename
     # Predictions on test set
     y_pred = nb_classifier.predict(X_test_vec)
 
-    # Count misclassifications
-    misclassified = (y_test != y_pred).sum()
-    total = len(y_test)
-    print(f"\nFile: {filename} | Vectorizer: {vectorizer_name}")
-    print(f"For {vectorizer}:\nNumber of misclassified examples out of {total} examples : {misclassified}")
+    # Extract misclassified examples
+    misclassified_indices = y_test != y_pred
+    misclassified_text = X_test[misclassified_indices]
+    true_labels = y_test[misclassified_indices]
+    predicted_labels = y_pred[misclassified_indices]
     
+    # Create list of dictionaries for misclassified examples
+    misclassified_data = []
+    for text, true_label, predicted_label in zip(misclassified_text, true_labels, predicted_labels):
+        misclassified_data.append({
+            'Text': text,
+            'True Label': int(true_label),  # Convert to int if necessary
+            'Predicted Label': int(predicted_label),  # Convert to int if necessary
+            'Vectorizer': vectorizer_name,
+            'File': filename
+        })
+    
+
+    print(f"Vectorizer: {vectorizer_name}\n")
     # Print classification report
+    print("\nClassification Report:")
+    classification_report_dict = classification_report(y_test, y_pred, output_dict=True)
     print(classification_report(y_test, y_pred))
-    return classification_report(y_test, y_pred, output_dict=True)
+    
+    # Count misclassifications
+    misclassified_count = misclassified_indices.sum()
+    total_examples = len(y_test)
+    print(f"Number of misclassified examples out of {total_examples} examples : {misclassified_count}")
+    print(50*"*")
+    
+    return classification_report_dict, misclassified_data, misclassified_count, total_examples
+
 
 # Function for training and evaluating FFNN model
 def train_and_evaluate_ffnn(model, train_loader, test_loader, criterion, optimizer, device):
     # Training
-    model.train()
+    model.to(device).train()
     for inputs, labels in tqdm(train_loader, desc="Training"):
         inputs, labels = inputs.to(device), labels.to(device)
         optimizer.zero_grad()
@@ -46,7 +69,7 @@ def train_and_evaluate_ffnn(model, train_loader, test_loader, criterion, optimiz
         optimizer.step()
     
     # Evaluation
-    model.eval()
+    model.to(device).eval()
     predictions = []
     true_labels = []
     with torch.no_grad():
@@ -57,15 +80,19 @@ def train_and_evaluate_ffnn(model, train_loader, test_loader, criterion, optimiz
             true_labels.extend(labels.cpu().detach().numpy())
     
     predictions = [1 if p >= 0.5 else 0 for p in predictions]
-    f1 = f1_score(true_labels, predictions)
-    print(f"\nF1 Score: {f1}")
     
+
+    f1 = f1_score(y_pred=predictions, y_true=true_labels)
+    print(f"F1: {f1}")
     # Classification report
     print(classification_report(true_labels, predictions))
+    # Count misclassified examples
+    misclassified_count = sum([1 for true, pred in zip(true_labels, predictions) if true != pred])
+    total_examples = len(true_labels)
     
-    # Confusion matrix
-    print("Confusion Matrix:")
-    print(confusion_matrix(true_labels, predictions))
+    print(f"\nNumber of misclassified examples out of {total_examples} examples : {misclassified_count}")
+    print(50*"*")
+
 
 def load_json_data(file_path):
     """Load JSON data from a file."""
@@ -79,9 +106,14 @@ def prepare_train_test_split(data, m, test_size=0.2, stratify_col='category', ra
         X = df[['preprocessed_text']]  # Features
     else:
         X = df['preprocessed_text']  # Features
-    y = df[stratify_col]           # Target variable
-    return train_test_split(X, y, test_size=test_size, stratify=y, random_state=random_state)
 
+    y = df[stratify_col]           # Target variable
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=test_size, stratify=y,
+                                                        random_state=random_state)
+    return X_train, X_test, y_train, y_test
+
+# Move samples from train set to test set
 def move_samples_between_sets(X_train, y_train, X_test, y_test, category_to_move=0, n_samples=750, random_state=42):
     """Move samples of a specific category from the train set to the test set."""
     # Identify indices of the specified category to move to the test set
